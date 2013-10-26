2bb129703884fbda10dd6d190cbd52f222e78d0b
Anand Soni <anand.92.soni@gmail.com>
2013-10-26 08:56:37 +0530

    Adding sql scripts

--- sqlscripts/script.cpp
+++ sqlscripts/script.cpp
@@ -7,37 +7,43 @@ using namespace std;
 
 int main() {
 	ifstream infile;
-	string comma=",";
 	infile.open("scripts.sql");
-	string line,ciname="spot";
+	string line,ciname="cuisine";
 	int j=1;
-	while (j<=57450) {
-		int nospots = rand() % 3;
-		int spotno = rand() % 50000;
-		string pi = "", std="";
-		for(int i=1; i<=nospots; i++) {
-			int type = rand() % 16 +1;
-			int num = spotno+i;
+	while (getline(infile, line)) {
+		//int	v2 = rand() % 100000;
+		//int v1 = rand() % 15 + 5;
+		//int	pin = 1000000;
+		//int stdcode = 10000;
+		//string pi = "", std="";
+		//for(int i=1; i<=v1; i++) {
+			//int num = i+v2;
 			ostringstream o,p,q;
-			o << pi << type;
-			string typeID = o.str();
-			p << pi << j;
-			string pinc = p.str();
-			q << ciname << num;
-			string stdc = q.str();
+			o << ciname << j;
+			string cuis = o.str();
+			//p << pi << pin;
+			//string pinc = p.str();
+			//q << std << stdcode;
+			//string stdc = q.str();
 			
-			string script = "INSERT INTO touristspots(cityid,spotname,typeid) VALUES (";
-			script.append(pinc);
+			string script = "INSERT INTO states VALUES (";
+			script.append(line);
+			string comma=",";
 			script.append(comma);
 			script.append("'");
-			script.append(stdc);
+			script.append(cuis);
 			script.append("'");
-			script.append(comma);
-			script.append(typeID);
+			//script.append(comma);
+			//script.append(pinc);
+			//script.append(comma);
+			//script.append(stdc);
 			script.append(");");
+			j++;
+			//pin++;
+			//stdcode++;
+			//stdcode = ((stdcode-10000) % 90000) + 10000;
 			cout<<script<<endl;
-		}
-		j++;
+		//}
 	}
 	return 0;
 }
